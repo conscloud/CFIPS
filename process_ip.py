a82ea5d230df32a9139e0845394736d383a24469
cmliu.net <admin@cmliu.net>
2023-09-28 18:48:47 +0800

    Update process_ip.py

--- process_ip.py
+++ process_ip.py
@@ -1,24 +1,37 @@
 import sys
+import os
 import ipaddress
 
-# 清空或创建ip0.txt文件
-with open("temp/ip0.txt", "w") as output_file:
-    pass
-
 # 函数：展开CIDR格式的IP地址范围并保存到ip0.txt
-def expand_cidr(cidr):
+def expand_cidr(cidr, output_file):
     ip_range = ipaddress.IPv4Network(cidr, strict=False)
-    with open("temp/ip0.txt", "a") as output_file:
-        for ip in ip_range:
-            output_file.write(str(ip) + "\n")
+    for ip in ip_range:
+        output_file.write(str(ip) + "\n")
 
 # 检查是否有命令行参数
 if len(sys.argv) != 2:
-    print("请提供一个CIDR格式的IP段作为参数")
+    print("请提供一个txt文件名作为参数")
     sys.exit(1)
 
-# 获取命令行参数并展开
-cidr_argument = sys.argv[1]
-expand_cidr(cidr_argument)
+# 获取命令行参数（文件名）
+txt_file = sys.argv[1]
 
-# print("处理完成，结果保存在temp/ip0.txt中")
+# 构建文件路径
+file_path = os.path.join("ASN", txt_file)
+
+# 检查文件是否存在
+if not os.path.exists(file_path):
+    print(f"找不到文件: {file_path}")
+    sys.exit(1)
+
+# 打开文件进行读取
+try:
+    with open(file_path, "r") as input_file, open("temp/ip0.txt", "w") as output_file:
+        for line in input_file:
+            line = line.strip()
+            # 假定每行都是一个CIDR格式的IP段
+            expand_cidr(line, output_file)
+
+    #print("处理完成，结果保存在temp/ip0.txt中")
+except Exception as e:
+    print(f"发生错误: {str(e)}")
