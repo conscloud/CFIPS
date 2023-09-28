d91f7aef5757afc7b5f5cba5a6800d2ed4abc542
cmliu.net <admin@cmliu.net>
2023-09-29 03:22:54 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -69,33 +69,21 @@ gogogo(){
 if [ -e "temp/ip0.txt" ]; then
     #echo "扫描IP文件库80端口开始..."
     ./Pscan -F temp/ip0.txt -P 80 -T $Threads -O temp/d80.txt -timeout 1s > /dev/null 2>&1
-else
-    echo "无有效IP内容，脚本终止。请重新编写ip.txt文件"
-    exit 1  # 终止脚本，1 表示出现了错误
 fi
 
 if [ -e "temp/d80.txt" ]; then
     #echo "扫描IP文件库80端口完成."
     awk 'NF' temp/d80.txt | sed 's/:80$//' >> temp/80.txt
-else
-    echo "无IP开启80端口，脚本终止。请增加ip.txt文件内IP数"
-    exit 1  # 终止脚本，1 表示出现了错误
 fi
 
 if [ -e "temp/80.txt" ]; then
     #echo "扫描IP文件库443端口开始..."
     ./Pscan -F temp/80.txt -P 443 -T $Threads -O temp/d443.txt -timeout 1s > /dev/null 2>&1
-else
-    echo "无IP开启443端口，脚本终止。请增加ip.txt文件内IP数"
-    exit 1  # 终止脚本，1 表示出现了错误
 fi
 
 if [ -e "temp/d443.txt" ]; then
     #echo "扫描IP文件库443端口完成."
     awk 'NF' temp/d443.txt | sed 's/:443$//' >> temp/443.txt
-else
-    echo "无IP开启443端口，脚本终止。请增加ip.txt文件内IP数"
-    exit 1  # 终止脚本，1 表示出现了错误
 fi
 
 #echo "开始验证CloudFlareIP"
