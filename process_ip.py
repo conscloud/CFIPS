1e0c0df290f210ca3e87c66c14ed164ee14da959
cmliu.net <admin@cmliu.net>
2023-09-30 01:22:29 +0800

    Update process_ip.py

--- process_ip.py
+++ process_ip.py
@@ -4,12 +4,14 @@ import ipaddress
 with open("temp/ip0.txt", "w") as output_file:
     pass
 
-# 函数：展开CIDR格式的IP地址范围并保存到ip0.txt
+# 定义一个空列表用于存储唯一的IP地址
+unique_ips = []
+
+# 函数：展开CIDR格式的IP地址范围并保存到unique_ips列表中
 def expand_cidr(cidr):
     ip_range = ipaddress.IPv4Network(cidr, strict=False)
-    with open("temp/ip0.txt", "a") as output_file:
-        for ip in ip_range:
-            output_file.write(str(ip) + "\n")
+    for ip in ip_range:
+        unique_ips.append(str(ip))
 
 # 读取ip.txt文件的每一行
 with open("ip.txt", "r") as input_file:
@@ -19,8 +21,16 @@ with open("ip.txt", "r") as input_file:
         if "/" in line:
             expand_cidr(line)
         else:
-            # 直接将IP追加到ip0.txt
-            with open("temp/ip0.txt", "a") as output_file:
-                output_file.write(line + "\n")
+            # 直接将IP添加到unique_ips列表中
+            unique_ips.append(line)
 
-#print("处理完成，结果保存在temp/ip0.txt中")
+# 去重并排序
+unique_ips = list(set(unique_ips))
+unique_ips.sort(key=lambda ip: ipaddress.ip_address(ip))
+
+# 将唯一的IP地址写回到ip0.txt文件中
+with open("temp/ip0.txt", "a") as output_file:
+    for ip in unique_ips:
+        output_file.write(ip + "\n")
+
+#print("处理完成，去重并排序的结果保存在temp/ip0.txt中")
