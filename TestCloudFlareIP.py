026245d4ae33e5117577990ed67c3198b566124d
cmliu.net <admin@cmliu.net>
2023-10-01 00:55:49 +0800

    Update TestCloudFlareIP.py

--- TestCloudFlareIP.py
+++ TestCloudFlareIP.py
@@ -6,10 +6,10 @@ import sys
 # 获取命令行参数
 TestCFIPDet = int(sys.argv[1])
 TestCFIPThreads = int(sys.argv[2])
-
+asnname = 'CloudFlareIP/{}'.format(sys.argv[3])
 # 读取ip.txt中的每个IP地址并执行测试
 def test_ip(ip):
-    max_retries = TestCFIPDet # 验证次数2~3
+    max_retries = TestCFIPDet  # 验证次数2~3
     retries = 0
     while retries < max_retries:
         try:
@@ -18,11 +18,11 @@ def test_ip(ip):
             # 检查是否是301跳转并且Server是cloudflare
             if response.status_code == 301 and 'cloudflare' in response.headers.get('Server', '').lower():
                 print(f"{datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} IP {ip} is CloudflareIP.")
-                with open('CloudFlareIP.txt', 'a') as cf_file:
+                with open(f'{asnname}', 'a') as cf_file:
                     cf_file.write(f"{ip}\n")
             break  # 如果测试成功，退出循环
         except Exception as e:
-            #print(f"IP {ip} 第 {retries + 1} 次测试出错: {str(e)}")
+            # print(f"IP {ip} 第 {retries + 1} 次测试出错: {str(e)}")
             retries += 1
 
 #print("开始测试。")
