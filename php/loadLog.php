a336a9931012fea608af1438a7374c6f5c01fcb1
cmliu.net <admin@cmliu.net>
2023-10-12 00:36:17 +0800

    Update loadLog.php

--- php/loadLog.php
+++ php/loadLog.php
@@ -1,8 +1,18 @@
 <?php
     $logFile = '../going.txt';
+    $linesToShow = 50;
+
     if(file_exists($logFile)){
-        $logContent = file_get_contents($logFile);
-        echo nl2br($logContent);
+        $file = new SplFileObject($logFile, 'r');
+        $file->seek(PHP_INT_MAX);
+        $totalLines = $file->key();
+
+        $linesToSkip = max(0, $totalLines - $linesToShow);
+        $file->seek($linesToSkip);
+
+        while (!$file->eof()) {
+            echo nl2br($file->fgets());
+        }
     } else {
         echo '无法找到日志文件。';
     }
