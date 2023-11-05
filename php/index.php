775756ede4c556d85f14eee927579fb9c3dada48
cmliu.net <admin@cmliu.net>
2023-11-06 02:28:35 +0800

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>CloudFlareIPScan</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <style>
         #scanLog {
